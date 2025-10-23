af261e9b37737839766b59199eebad1504a1e54e
sytyler2008-eng <sytyler2008@gmail.com>
2025-10-23 11:52:58 -0500

    made a ringbuffer class

---
+++ src/RingBuffer.java
@@ -0,0 +1,41 @@
+import java.io.*;
+
+public class RingBuffer
+{
+	double[] buffer = new double[10];
+	
+	public RingBuffer(int capacity)
+	{
+		
+	}
+	
+	public int size()
+	{
+		
+	}
+	
+	public boolean isEmpty()
+	{
+		
+	}
+	
+	public boolean isFull()
+	{
+		
+	}
+	
+	public void enqueue(double x)
+	{
+		
+	}
+	
+	public double peek()
+	{
+		
+	}
+	
+	public String toString()
+	{
+		
+	}
+}
